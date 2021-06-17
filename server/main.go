fd0986dc25ec1e32ce3c33a95c5671d6556f71b9
kk103467 <kk43502forengineering10479@gmail.com>
2021-06-17 20:16:51 +0900

    created todo model

--- server/main.go
+++ server/main.go
@@ -7,9 +7,9 @@ import (
 	"github.com/kk103467/go_react_todo/server/router"
 )
 
-func main()  {
+func main() {
 	r := router.MyMux()
 
 	// Bind to a port and pass our router in
-    log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
--- server/router/router.go
+++ server/router/router.go
@@ -1,13 +1,14 @@
 package router
 
-import(
-	"net/http"
+import (
 	"github.com/gorilla/mux"
+	"net/http"
 
 	"github.com/kk103467/go_react_todo/server/handlers"
 )
+
 func MyMux() http.Handler {
 	r := mux.NewRouter()
-    r.HandleFunc("/", handlers.HomeHandler)
+	r.HandleFunc("/", handlers.HomeHandler)
 	return r
 }
