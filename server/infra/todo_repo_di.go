7f8df3c54f45907610eab19bd1065c18661a3f19
kk103467 <kk43502forengineering10479@gmail.com>
2021-06-17 22:42:17 +0900

    implemented New todorepo

--- server/infra/todo_repo_di.go
+++ server/infra/todo_repo_di.go
@@ -1,8 +1,15 @@
 package infra
 
-import "github.com/kk103467/go_react_todo/server/domain/model"
+import (
+	"github.com/kk103467/go_react_todo/server/domain/model"
+	"github.com/kk103467/go_react_todo/server/domain/repository"
+)
 
 type todoRepo struct {}
+
+func NewTodoRepo() repository.TodoRepo {
+	return &todoRepo{}
+}
 
 func (tr *todoRepo) GetAll() ([]model.Todo, error) {
 	todo1 := model.Todo{}
--- server/main.go
+++ server/main.go
@@ -4,11 +4,16 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/kk103467/go_react_todo/server/infra"
 	"github.com/kk103467/go_react_todo/server/router"
 )
 
 func main() {
 	r := router.MyMux()
+
+	todoRepo := infra.NewTodoRepo()
+
+	
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
