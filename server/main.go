185662a236f4e26be0e6f06a01155e7e002c6b7d
kk103467 <kk43502forengineering10479@gmail.com>
2021-06-23 15:42:04 +0900

    changed directory structure

--- server/main.go
+++ server/main.go
@@ -5,13 +5,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/kk103467/go_react_todo/server/infra/di"
+	"github.com/kk103467/go_react_todo/server/infra"
 	"github.com/kk103467/go_react_todo/server/presentation"
 	"github.com/kk103467/go_react_todo/server/usecase"
 )
 
 func main() {
-	todoRepo := di.NewTodoRepo()
+	todoRepo := infra.NewTodoRepo()
 	todoUsecase := usecase.NewTodoUsecase(todoRepo)
 	todoHandler := presentation.NewTodoHandler(todoUsecase)
 
