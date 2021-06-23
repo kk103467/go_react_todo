f1284c97aa6713a7b0fb61689d200994d7846d2f
kk103467 <kk43502forengineering10479@gmail.com>
2021-06-23 16:28:23 +0900

    changed url

--- server/presentation/router.go
+++ server/presentation/router.go
@@ -9,8 +9,8 @@ func MyMux(th TodoHandler) http.Handler {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/api", HomeHandler)
-	r.HandleFunc("/api/view", th.ViewHandler)
-	r.HandleFunc("/api/add", th.AddHandler)
+	r.HandleFunc("/api/todos", th.ViewHandler).Methods("GET")
+	r.HandleFunc("/api/todo", th.AddHandler).Methods("POST")
 	
 	return r
 }
